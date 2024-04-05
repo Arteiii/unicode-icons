698058dc9b181c924908f71ffbe96c723d39ec9e
Ben <ben.arteii@proton.me>
2024-04-06 00:03:22 +0200

    added features

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -1,10 +1,29 @@
+#[cfg(feature = "activities")]
 pub mod activities;
+
+#[cfg(feature = "animals_and_nature")]
 pub mod animals_and_nature;
+
+#[cfg(feature = "component")]
 pub mod component;
+
+#[cfg(feature = "flags")]
 pub mod flags;
+
+#[cfg(feature = "food_and_drink")]
 pub mod food_and_drink;
+
+#[cfg(feature = "objects")]
 pub mod objects;
+
+#[cfg(feature = "people_and_body")]
 pub mod people_and_body;
+
+#[cfg(feature = "smileys_and_emotion")]
 pub mod smileys_and_emotion;
+
+#[cfg(feature = "symbols")]
 pub mod symbols;
+
+#[cfg(feature = "travel_and_places")]
 pub mod travel_and_places;
